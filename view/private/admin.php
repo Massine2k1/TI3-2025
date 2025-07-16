018c1bb8a09cf951931d5a0362b3581e5853b614
Massine <massineabgar@hotmail.com>
2025-07-16 15:41:19 +0200

    tableau responsive

--- view/private/admin.php
+++ view/private/admin.php
@@ -8,7 +8,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet" />
   <link rel="stylesheet" href="css/form.css">
-  <link rel="stylesheet" href="css/input.css">
+  <link rel="stylesheet" href="css/admin.css">
 <body class="bg-dark text-light">
 
   <?php require "menu.php"; ?>
@@ -79,6 +79,40 @@
           </tbody>
         </table>
       </div>
+      <?php
+      foreach ($localisations as $l):
+      ?>
+      <div class="card text-bg-secondary bg-opacity-25 mb-3">
+        <div class="card-header bg-secondary bg-opacity-25">
+          <strong>Nom</strong> : <span class="nom"><?=$l['nom']?></span>
+        </div>
+        <div class="card-body">
+          <card class="card-text">
+            <strong>Adresse :</strong> <?=$l['adresse']?><?= $l['numero'] ?>
+          </card>
+          <div class="card-text">
+            <strong>Ville</strong> : <?=$l['codepostal']?> <?= $l['ville'] ?>
+          </div>
+          <div class="card-text mb-3">
+            <strong>latitude : </strong> <?=$l['latitude']?> | <strong>longitude :</strong> <?=$l['longitude']?>
+          </div>
+          <div class="row">
+            <div class="col-6">
+              <a onclick="if(confirm('Voulez-vous vraiment supprimer l\'article : <?= addslashes(html_entity_decode($l['nom'])); ?> ?')){window.location.href='./?pg=delete&idLocalisation=<?= $l['id'] ?>';}" class="btn btn-danger btn-sm rounded-3 w-100">
+                Supprimer
+              </a>
+            </div>
+            <div class="col-6">
+              <a href="./?pg=update&idLocalisation=<?= $l['id'] ?>" class="btn btn-success btn-sm rounded-3 w-100">
+                    Modifier
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <?php
+      endforeach;
+      ?>
   </div>
      <?php require "footer.php"; ?>
      
@@ -88,6 +122,7 @@
 
         const search = document.getElementById('input');
         const tableRows = document.querySelectorAll('tbody tr');
+        const card = document.querySelectorAll('.card');
         
         search.addEventListener('input', function() {
           const searchValue = search.value.trim().toLowerCase();
@@ -102,6 +137,19 @@
               row.style.display = 'none';
             }
           });
+
+          card.forEach(element=>{
+            const divName = element.querySelector('.nom');
+            const NameContent = divName.textContent.toLowerCase();
+
+            if(NameContent.startsWith(searchValue)){
+              element.style.display = '';
+            }else{
+              element.style.display = 'none';
+            }
+          })
+                
+
         });
       });
 </script>
